3bc1dc134043d03e9daab91a33d80bf2db865dbe
Siris <97780529+Siris4@users.noreply.github.com>
2024-01-10 14:48:34 -0600

    Add files via upload

---
+++ _24_0020__Life_W_D16_v02_f01.py
@@ -0,0 +1,6 @@
+import math
+theAnswer = 42
+
+def quote_einstein():
+    print('The true sign of intelligence is not knowledge, but imagination')
+
---
+++ _24_0020__Quote_App_Main_W_D16_v02_f01.py
@@ -0,0 +1,11 @@
+import _24_0020__Life_W_D16_v02_f01
+import math
+
+path = _24_0020__Life_W_D16_v02_f01
+
+value = _24_0020__Life_W_D16_v02_f01.theAnswer
+
+print(math.pi)
+print(value)
+path.quote_einstein()
+print(path.quote_einstein())
